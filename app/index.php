313f26612469b19435aa136bbcad9c3ce03b31c6
Davi Palazzin Pereira <115328246+DaviPalazzin@users.noreply.github.com>
2025-03-20 01:48:46 +0000

    Aula 19/03/2025

--- app/index.php
+++ app/index.php
@@ -1,8 +1,51 @@
 <?php
-// require './vendor/autoload.php';
 
-// use Application\DBConnection\MySQLConnection;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-// $db = new MySQLConnection();
+use Slim\Views\PhpRenderer;
 
-echo phpinfo();
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get(
+    '/',
+    function (
+        Request $request,
+        Response $response,
+        $args
+    ) {
+        $response->getBody()->write("Bom Dia!!!");
+        return $response;
+    }
+);
+
+$app->get(
+    '/mensagem/{nome}',
+    function (
+        Request $request,
+        Response $response,
+        $args
+    ) {
+        $response->getBody()->write("Boa Noite!!!" . $args['nome']);
+        return $response;
+    }
+);
+
+$app->get(
+    '/hello',
+    function (
+        Request $request,
+        Response $response,
+        $args
+    ) {
+        $renderer = new PhpRenderer(__DIR__ . '/templates');
+        $msg = 'Nova Mensagem';
+        return $renderer->render($response, 'hello.php', ['mensagem' => $msg]);
+    }
+);
+
+
+$app->run();
